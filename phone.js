f9fa25cb24c0574741b2afad0019a5bfd81c0e85
j-azr <azrakjad@gmail.com>
2024-08-14 15:44:38 -0400

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'a3q3.html'));
+});
+
+app.post('/submit', (req, res) => {
+    const { name, phone } = req.body;
+    const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
+
+    if (phoneRegex.test(phone)) {
+        res.send(`Thank you, ${name}. Your phone number ${phone} is valid.`);
+    } else {
+        res.send(`Sorry, ${name}. The phone number ${phone} is not in the correct format.`);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
